b479fec9de6b1da674c605dc4df43f7b77c6eb80
Filip Gorczyca <filipekbwo@wp.pl>
2022-04-13 14:45:31 +0200

    Zmiany wprowadzone na spotkaniu 13.04

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : Entity
@@ -7,13 +8,21 @@ public class PlayerController : Entity
     [Header("Komponenty")]
     [SerializeField] private Rigidbody2D rb;
 
+    [Header("Obiekty")]
+    public GameObject Phone;
+    public GameObject canvas;
+
     [Header("G³ówne wartoœci")]
     public float speed;
+    public float sprintSpeed;
 
     [Header("Debug")]
+    [ReadOnly] public float currentSpeed;
     [ReadOnly] public Vector2 inputDirection;
     [ReadOnly] public Vector2 mousePosition;
     [ReadOnly] public Vector2 aimDirection;
+
+    private readonly float _lerpBetweenSpeed = 2f;
 
 
     protected override void Awake()
@@ -23,17 +32,44 @@ public class PlayerController : Entity
             instance = this;
         else
             Destroy(gameObject);
+
+        currentSpeed = speed;
     }
     protected override void Update()
     {
         HandleMovement();
+        HandleInput();
 
         mousePosition = Utils.GetMouseWorldPosition();
         aimDirection = (mousePosition - (Vector2)transform.position).normalized;
     }
     protected override void FixedUpdate()
     {
-        rb.MovePosition((Vector2)transform.position + speed * Time.deltaTime * inputDirection);
+        rb.MovePosition(rb.position + currentSpeed * Time.deltaTime * inputDirection);
+    }
+
+    /// <summary>
+    /// Metoda zawieraj¹ca wszystkie odwolania do klawiatury
+    /// np. Esc - jako wejscie do menu
+    /// </summary>
+    private void HandleInput()
+    {
+        //Na kliknieciu escape aktywujemy i dezaktywujemy Telefon
+        if (Input.GetKeyDown(KeyCode.Escape)) Phone.SetActive(!Phone.activeSelf);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            currentSpeed = Mathf.Lerp(currentSpeed, sprintSpeed, _lerpBetweenSpeed * Time.deltaTime);
+        else
+            currentSpeed = Mathf.Lerp(currentSpeed, speed, _lerpBetweenSpeed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Tymczasowe metoda w Player Controllerze ktora odpowiada za Dezaktywacje i aktywacje main menu z poziomu telefonu
+    /// </summary>
+    public void OpenCloseMainMenu()
+    {
+        canvas.SetActive(!canvas.activeSelf);
+        Time.timeScale = canvas.activeSelf ? 0 : 1;
     }
 
     private void HandleMovement()
